e85118b9076dd6f10dc6c90285958a210603f345
Debasis Saikia <debasishsaikia136@gmail.com>
2021-07-14 20:33:20 +0530

    header component build

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Header from './components/Header'
 import { StyleSheet, View, Text } from 'react-native';
 
 export default function App() {
 
   return (
-    <View >
-      <Text>Hello</Text>
+    <View style={styles.screen}>
+      <Header title="Number Game" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1,
+  }
 });
---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Header = ({ title }) => {
+    return (
+        <View style={styles.header}>
+            <Text style={styles.headText}>{title}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    header: {
+        width: '100%',
+        height: 90,
+        paddingTop: 36,
+        backgroundColor: "#d35400",
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    headText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold'
+    }
+});
+
+export default Header;
